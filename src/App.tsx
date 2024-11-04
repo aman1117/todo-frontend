c2bc21458b5117d6940d4ff0044bbe6972abd986
aman1117 <arl0817osho@gmail.com>
2024-11-04 23:04:03 +0000

    added todos to cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import { Square } from './components/square';
-
+import Cookies from 'js-cookie';
 
 function App() {
   const [todo, setTodo] = useState<string>("");
   const [todoList, setTodoList] = useState<string[]>([]);
   const [doneCount, setDoneCount] = useState<number>(1);
 
+  useEffect(() => {
+    const storedTodoList = Cookies.get('todoList');
+    if (storedTodoList) {
+      setTodoList(JSON.parse(storedTodoList));
+    }
+  }, []);
 
   const handleSubmit = () => {
     if (todo.trim() !== "") {
+      const updatedTodoList = [...todoList, todo];
       setTodoList([...todoList, todo]);
       setTodo("");
+      Cookies.set('todoList', JSON.stringify(updatedTodoList));
     }
   }
 
   return (
     <div className="w-screen h-screen flex  justify-center bg-pink-50">
-      <div className='w-96 bg-slate-50'>
+      <div className='min-w-96 bg-slate-50'>
         <p className='font-bold text-xl'>
           Todos
         </p>
-        <div className='flex'>
-          {
-            [...Array(doneCount)].map(() => <Square value={true} />)
-          }
-          {
-            todoList.map(() => <Square value={false} />)
-          }
 
-        </div>
 
         <p>
           Add tasks to your list
@@ -49,6 +49,15 @@ function App() {
             }
           />
           <button className='m-2 bg-green-400' onClick={handleSubmit}>Add</button>
+        </div>
+        <div className='flex'>
+          {
+            [...Array(doneCount)].map(() => <Square value={true} />)
+          }
+          {
+            todoList.map(() => <Square value={false} />)
+          }
+
         </div>
 
         <div>
---
+++ src/types/js-cookie.d.ts
@@ -0,0 +1,4 @@
+declare module 'js-cookie' {
+  const JsCookie: any;
+  export default JsCookie;
+}
